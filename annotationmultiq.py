7eabf78af66becc28432554085522a780b457b63
CTAN210 <CTAN210@e.ntu.edu.sg>
2022-11-12 15:57:31 +0800

    fully integrated annotationmultiq.py to interface.py

--- annotationmultiq.py
+++ annotationmultiq.py
@@ -8,9 +8,9 @@ import turtle
 #                   "C.c_custkey = O.o_custkey": ["This is the second annotation that is a bit longer than the other ones and spans more than one line"],
 #                   "A = B": ["This is the third annotation"], "orders O": ["This is the 4th one"]}
 
-query = "SELECT n_name FROM nation, region,supplier WHERE r_regionkey=n_regionkey AND s_nationkey = n_nationkey AND n_name IN (SELECT DISTINCT n_name FROM nation,region WHERE r_regionkey=n_regionkey AND r_name <> 'AMERICA') AND r_name in (SELECT DISTINCT r_name from region where r_name <> 'ASIA')"
-annotationList = {"r_regionkey=n_regionkey": ["First annotation"],
-                  "r_name <> 'AMERICA'": ["Second annotation"], "region": ["Third annotation", "Fourth annotation", "Fifth annotation"]}
+# query = "SELECT n_name FROM nation, region,supplier WHERE r_regionkey=n_regionkey AND s_nationkey = n_nationkey AND n_name IN (SELECT DISTINCT n_name FROM nation,region WHERE r_regionkey=n_regionkey AND r_name <> 'AMERICA') AND r_name in (SELECT DISTINCT r_name from region where r_name <> 'ASIA')"
+# annotationList = {"r_regionkey=n_regionkey": "First annotation",
+#                   "r_name <> 'AMERICA'": ["Second annotation"], "region": ["Third annotation", "Fourth annotation", "Fifth annotation"]}
 
 # query = "select * from part where p_brand = 'Brand#13' and p_size <> (select max(p_size) from part);"
 # annotationList = {"part": ["This is the annotation for the part in the outer query",
@@ -18,16 +18,19 @@ annotationList = {"r_regionkey=n_regionkey": ["First annotation"],
 
 from db import DBConnection
 class Annotator:
-    def __init__(self):
-        # Pre-processing of data
-        # Find indexes of annotees
-        tquery = query
+    def __init__(self,query, annotationList):
+        self.annotationList = annotationList
+        self.query = query
+    
+    def annotation_matcher(self):
+        # Pre-processing of query in order to return the indexes of the annotee
+        tquery = self.query
         specList = ["(", ")", " ", ",", "\0"]
         idsort = []
         annoidx = {}  # "annotee index number" : "annotee"
 
         # 1) Sort the annotatees by increasing length
-        keyList = list(annotationList.keys())
+        keyList = list(self.annotationList.keys())
         keyList.sort(key=len, reverse=True)
         print("1) Annotees sorted: " + str(keyList))
         print()
@@ -37,9 +40,12 @@ class Annotator:
         #    Identify the indexes of the annotees, checking that they do not overlap with previous ones
         #    For each annotee, check that the left and write of it are either a space, comma, or bracket, and not other letters
         for k in keyList:
-            print("Finding indexes for " + k)
-            count = len(annotationList[k])
-            print("Start count: " + str(count))
+            # print("Finding indexes for " + k)
+            if type(self.annotationList[k]) == list: # If the value is a list
+                count = len(self.annotationList[k])
+            else:
+                count = 1
+            # print("Start count: " + str(count))
             strt = 0
             while count > 0:  # While there are still instances of k not found yet
                 # print("Count: " + str(count))
@@ -59,17 +65,17 @@ class Annotator:
                     tquery = tquery[:tindex] + " " * len(k) + tquery[(tindex+len(k)):]
                     count -= 1  # Reduce count by 1
         idsort.sort()
-        print("Indexes of annotees in query found:")
-        print(idsort)
-        print(annoidx)
-        print()
+        # print("Indexes of annotees in query found:")
+        # print(idsort)
+        # print(annoidx)
+        # print()
 
         # 3) Iterate through the annotation indexes and split the query into words accordingly.
         #    Record the indexes of the words with annotations, to be detected later during printing
         wordList = []  # List of words in the query
         wordannoidx = []  # List of the indexes of annotated words based on the word list
         idx = 0
-        tquery = query
+        tquery = self.query
 
         for tid in idsort:
             if tquery[idx:tid-1]:
@@ -82,87 +88,86 @@ class Annotator:
         tlist = tquery[idx:].split()
         wordList.extend(tlist)
 
-        print("Word list created, indexes of annotees identified")
-        print("Word list: " + str(wordList))
-        print("Annotee indexes: " + str(wordannoidx))
-        print()
+        # print("Word list created, indexes of annotees identified")
+        # print("Word list: " + str(wordList))
+        # print("Annotee indexes: " + str(wordannoidx))
+        # print()
 
-        Annotator.turtle_drawer(self,wordannoidx,wordList)
+        return wordannoidx, wordList
     
-    def turtle_drawer(self, a,b):
-        wordannoidx = a
-        wordList = b
-        # print(" Starting turtle")
-        # Set up window and pen
-        wheight = 604
-        wwidth = 1204
+    def turtle_drawer(self, wordannoidx, wordList, annotationList):
+
         ftsz = 16
         ft = ("Courier", ftsz)
         aftsz = 12
         aft = ("Courier", aftsz)
-        # window = turtle.Screen()
-        # window.setup(width=wwidth, height=wheight)
-        pen = turtle.Turtle()
-        pen.speed(0)
-        pen.shape("turtle")
 
+        self.pen = turtle.RawTurtle(self.screen,shape = "turtle")
+        self.pen.color("green")
+        self.pen.speed(0)
+
+        wheight, wwidth = 604, 1204
         interv = wheight/len(wordannoidx)
         annopos = (1/2) * wheight - 18
 
-        pen.penup()
-        pen.setposition(-(1/2)*wwidth + 5, (1/7)*wheight)
-        pen.setheading(0)
+        self.pen.penup()
+        self.pen.setposition(-(1/2)*wwidth + 5, (1/7)*wheight)
+        self.pen.setheading(0)
+
         for i in range(len(wordList)):  # Iterate through word list
             if wordannoidx and i == wordannoidx[0]:  # HIGHLIGHTING
-                pen.color("yellow")
-                pen.pendown()
-                pen.begin_fill()
+                self.pen.color("yellow")
+                self.pen.pendown()
+                self.pen.begin_fill()
                 for k in range(2):
-                    pen.forward(len(wordList[i]) * 14)
-                    pen.left(90)
-                    pen.forward(16)
-                    pen.left(90)
-                pen.end_fill()
-                pen.penup()
-                pen.color("black")
-            pen.write(wordList[i], font=ft)
-            pen.forward(len(wordList[i]) * 14)
+                    self.pen.forward(len(wordList[i]) * 14)
+                    self.pen.left(90)
+                    self.pen.forward(16)
+                    self.pen.left(90)
+                self.pen.end_fill()
+                self.pen.penup()
+                self.pen.color("black")
+            self.pen.write(wordList[i], font=ft)
+            self.pen.forward(len(wordList[i]) * 14)
             # For annotating
             if wordannoidx and i == wordannoidx[0]:  # ANNOTATION FOUND
                 # Writing and pointing
-                curpos = pen.pos()
-                pen.setheading(90)
-                pen.forward(16)
-                pen.setheading(180)
-                pen.forward(len(wordList[i]) * 0.5*14)
-                pen.setheading(0)
-                pen.pendown()
-                pen.color("blue")
-                pen.setposition((1/6)*wwidth + 5, annopos)
-                pen.penup()
-                pen.color("red")
+                curpos = self.pen.pos()
+                self.pen.setheading(90)
+                self.pen.forward(16)
+                self.pen.setheading(180)
+                self.pen.forward(len(wordList[i]) * 0.5*14)
+                self.pen.setheading(0)
+                self.pen.pendown()
+                self.pen.color("blue")
+                self.pen.setposition((1/6)*wwidth + 5, annopos)
+                self.pen.penup()
+                self.pen.color("red")
                 annopos -= interv
                 # PRINTNG OUT THE ANNOTATIONS
                 # print(annotationList[wordList[i]])
-                ttext = annotationList[wordList[i]][0]
                 # print("Annotation to be printed: " + ttext)
-                annotationList[wordList[i]].pop(0)
+                if type(annotationList[wordList[i]]) == list:
+                    ttext = annotationList[wordList[i]][0]
+                    annotationList[wordList[i]].pop(0)
+                else:
+                    ttext = annotationList[wordList[i]]
                 annotxtlist = ttext.split()
                 # print(annotxtlist)
                 for j in range(len(annotxtlist)):
-                    pen.write(annotxtlist[j], font=aft)
-                    pen.forward(len(annotxtlist[j])*10 + 5)
-                    if (j != (len(annotxtlist)-1)) and (pen.pos()[0] + len(annotxtlist[j+1])*10 >= (1/2)*wwidth):
-                        pen.setheading(270)
-                        pen.forward(aftsz+5)
-                        pen.setposition((1/6)*wwidth + 5, pen.pos()[1])
-                        pen.setheading(0)
-                pen.setposition(curpos)
-                pen.color("black")
+                    self.pen.write(annotxtlist[j], font=aft)
+                    self.pen.forward(len(annotxtlist[j])*10 + 5)
+                    if (j != (len(annotxtlist)-1)) and (self.pen.pos()[0] + len(annotxtlist[j+1])*10 >= (1/2)*wwidth):
+                        self.pen.setheading(270)
+                        self.pen.forward(aftsz+5)
+                        self.pen.setposition((1/6)*wwidth + 5, self.pen.pos()[1])
+                        self.pen.setheading(0)
+                self.pen.setposition(curpos)
+                self.pen.color("black")
                 wordannoidx.pop(0)
             # If next word is not comma
             if i != (len(wordList)-1) and wordList[i+1] != ",":
-                pen.forward(10)
+                self.pen.forward(10)
             if i != (len(wordList)-1):
                 tempword = wordList[i+1].lower()
                 if "(" in tempword:
@@ -170,15 +175,15 @@ class Annotator:
                     tempword = tempword[1:]
                     # print("Unbracketed: " + tempword)
             if (i != (len(wordList)-1) and tempword in ["select", "from", "where"])\
-                    or (i != (len(wordList)-1) and (pen.pos()[0] + (len(wordList[i+1]) * 14)) >= (1/6)*wwidth):
+                    or (i != (len(wordList)-1) and (self.pen.pos()[0] + (len(wordList[i+1]) * 14)) >= (1/6)*wwidth):
                 # print("Next line for tempword: " + tempword)
-                pen.setheading(270)
-                pen.forward(ftsz+5)
-                pen.setposition(-(1/2) * wwidth + 5, pen.pos()[1])
-                pen.setheading(0)
+                self.pen.setheading(270)
+                self.pen.forward(ftsz+5)
+                self.pen.setposition(-(1/2) * wwidth + 5, self.pen.pos()[1])
+                self.pen.setheading(0)
 
-        pen.setposition(wwidth, wheight)
-        print("Turtle finished")
-        window.exitonclick()
+        # pen.setposition(wwidth, wheight)
+        # print("Turtle finished")
+        # window.exitonclick()
 
-        turtle.done
+        # turtle.done
--- new_interface.py
+++ new_interface.py
@@ -17,7 +17,6 @@ class UserInterface:
         self.label = tk.Label(root, text = "Enter your Query to optimize")
         
         
-        
         self.label.config(font = ("Segoe",12))
         self.label.pack()
         self.textfield.pack()
@@ -30,114 +29,23 @@ class UserInterface:
         print(self.screen.window_height(), self.screen.window_width())
         # self.canvas.create_window(300, 300, window = self.button)
         
-
-    def do_stuff(self):
-        for color in ["red", "yellow", "green"]:
-            self.my_lovely_turtle.color(color)
-            self.my_lovely_turtle.right(120)
     
     def get_input(self):
         query = self.textfield.get("1.0","end-1c")
-        print(query)
+        # print(query)
         self.label1 = tk.Label(root, text = query)
         self.canvas.create_window(200,230, window = self.label1)
 
-        self.turtle_drawer()
+        query = "select * from customer C, orders O where C.c_custkey = O.o_custkey and A = B"
+        annotationList = {"customer C": ["This is the first annotation"],
+                        "C.c_custkey = O.o_custkey": ["This is the second annotation that is a bit longer than the other ones and spans more than one line"],
+                        "A = B": ["This is the third annotation"], "orders O": ["This is the 4th one"]}
 
-    def turtle_drawer(self):
-        ftsz = 16
-        ft = ("Courier", ftsz)
-        aftsz = 12
-        aft = ("Courier", aftsz)
-
-        self.pen = turtle.RawTurtle(self.screen,shape = "turtle")
-        self.pen.color("green")
-        self.pen.speed(0)
-
-        query = "SELECT n_name FROM nation, region,supplier WHERE r_regionkey=n_regionkey AND s_nationkey = n_nationkey AND n_name IN (SELECT DISTINCT n_name FROM nation,region WHERE r_regionkey=n_regionkey AND r_name <> 'AMERICA') AND r_name in (SELECT DISTINCT r_name from region where r_name <> 'ASIA')"
-        annotationList = {"r_regionkey=n_regionkey": ["First annotation"],
-                  "r_name <> 'AMERICA'": ["Second annotation"], "region": ["Third annotation", "Fourth annotation", "Fifth annotation"]}
-
-
-        wordList = ['SELECT', 'n_name', 'FROM', 'nation,', 'region', ',supplier', 'WHERE', 'r_regionkey=n_regionkey', 'AND', 's_nationkey', '=', 'n_nationkey', 'AND', 'n_name', 'IN', '(SELECT', 'DISTINCT', 'n_name', 'FROM', 'nation', 'region', 'WHERE', 'r_regionkey=n_regionkey', 'AND', "r_name <> 'AMERICA'", ')', 'AND', 'r_name', 'in', '(SELECT', 'DISTINCT', 'r_name', 'from', 'region', 'where', 'r_name', '<>', "'ASIA')"]
-        wordannoidx = [4, 7, 20, 24, 33]
-
-        wheight, wwidth = 604, 1204
-        interv = wheight/len(wordannoidx)
-        annopos = (1/2) * wheight - 18
-
-        self.pen.penup()
-        self.pen.setposition(-(1/2)*wwidth + 5, (1/7)*wheight)
-        self.pen.setheading(0)
-
-        for i in range(len(wordList)):  # Iterate through word list
-            if wordannoidx and i == wordannoidx[0]:  # HIGHLIGHTING
-                self.pen.color("yellow")
-                self.pen.pendown()
-                self.pen.begin_fill()
-                for k in range(2):
-                    self.pen.forward(len(wordList[i]) * 14)
-                    self.pen.left(90)
-                    self.pen.forward(16)
-                    self.pen.left(90)
-                self.pen.end_fill()
-                self.pen.penup()
-                self.pen.color("black")
-            self.pen.write(wordList[i], font=ft)
-            self.pen.forward(len(wordList[i]) * 14)
-            # For annotating
-            if wordannoidx and i == wordannoidx[0]:  # ANNOTATION FOUND
-                # Writing and pointing
-                curpos = self.pen.pos()
-                self.pen.setheading(90)
-                self.pen.forward(16)
-                self.pen.setheading(180)
-                self.pen.forward(len(wordList[i]) * 0.5*14)
-                self.pen.setheading(0)
-                self.pen.pendown()
-                self.pen.color("blue")
-                self.pen.setposition((1/6)*wwidth + 5, annopos)
-                self.pen.penup()
-                self.pen.color("red")
-                annopos -= interv
-                # PRINTNG OUT THE ANNOTATIONS
-                # print(annotationList[wordList[i]])
-                ttext = annotationList[wordList[i]][0]
-                # print("Annotation to be printed: " + ttext)
-                annotationList[wordList[i]].pop(0)
-                annotxtlist = ttext.split()
-                # print(annotxtlist)
-                for j in range(len(annotxtlist)):
-                    self.pen.write(annotxtlist[j], font=aft)
-                    self.pen.forward(len(annotxtlist[j])*10 + 5)
-                    if (j != (len(annotxtlist)-1)) and (self.pen.pos()[0] + len(annotxtlist[j+1])*10 >= (1/2)*wwidth):
-                        self.pen.setheading(270)
-                        self.pen.forward(aftsz+5)
-                        self.pen.setposition((1/6)*wwidth + 5, self.pen.pos()[1])
-                        self.pen.setheading(0)
-                self.pen.setposition(curpos)
-                self.pen.color("black")
-                wordannoidx.pop(0)
-            # If next word is not comma
-            if i != (len(wordList)-1) and wordList[i+1] != ",":
-                self.pen.forward(10)
-            if i != (len(wordList)-1):
-                tempword = wordList[i+1].lower()
-                if "(" in tempword:
-                    # print("Bracketed word found:" + tempword)
-                    tempword = tempword[1:]
-                    # print("Unbracketed: " + tempword)
-            if (i != (len(wordList)-1) and tempword in ["select", "from", "where"])\
-                    or (i != (len(wordList)-1) and (self.pen.pos()[0] + (len(wordList[i+1]) * 14)) >= (1/6)*wwidth):
-                # print("Next line for tempword: " + tempword)
-                self.pen.setheading(270)
-                self.pen.forward(ftsz+5)
-                self.pen.setposition(-(1/2) * wwidth + 5, self.pen.pos()[1])
-                self.pen.setheading(0)
-
-    def press(self):
-        self.do_stuff()
-
+        self.instance = Annotator(query,annotationList)
+        wordannoidx, wordList = Annotator.annotation_matcher(self.instance)
+ 
+        Annotator.turtle_drawer(self, wordannoidx, wordList, annotationList)
+    
 
 if __name__ == '__main__':
     root = tk.Tk()
